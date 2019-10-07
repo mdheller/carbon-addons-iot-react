f6bc8ff7728f552a448be26d57f80a0c4c9e5745
Taylor Jones <taylor.jones826@gmail.com>
2019-10-07 16:11:31 -0500

    fix(webpack): no longer need custom css loader, storybook has one built in. migrate previous webpack config plugins/devtool

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,10 +1,20 @@
 const path = require('path');
+const webpack = require('webpack');
 
 // Export a function. Accept the base config as the only param.
 module.exports = async ({ config, mode }) => {
   // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
   // You can change the configuration based on that.
   // 'PRODUCTION' is used when building the static version of storybook.
+
+  config.devtool = mode === 'DEVELOPMENT' ? '#cheap-module-eval-source-map' : 'source-map';
+
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    })
+  );
+  config.plugins.push(new webpack.IgnorePlugin(/\.\/locale$/));
 
   config.module.rules.push({
     test: /\.(js|jsx)$/,
@@ -13,7 +23,7 @@ module.exports = async ({ config, mode }) => {
   });
 
   config.module.rules.push({
-    test: /\.s?css$/,
+    test: /\.scss$/,
     exclude: [/coverage/],
     use: [
       { loader: 'style-loader' },
